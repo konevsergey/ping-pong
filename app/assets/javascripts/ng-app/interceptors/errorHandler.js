0d12c42b04df739c026cd9e5935d977adbdea0a2
Konev Sergey <konevsergey111@gmail.com>
2015-12-29 11:43:02 +0200

    .

--- app/assets/javascripts/ng-app/interceptors/errorHandler.js
+++ app/assets/javascripts/ng-app/interceptors/errorHandler.js
@@ -1,10 +1,8 @@
 function ErrorHandlerInterceptor($q, $rootScope) {
   return {
-    response: function(response) {
-      console.log("response: ")
-      console.log(response);
-      return response;
-    },
+    // response: function(response) {
+    //   return response;
+    // },
     responseError: function(response) {
       $rootScope.$emit('responseError', response);
       return $q.reject(response);
